8bba147c3aebe955e52280a1ca6036fe76abee3d
Julia Lameijer <julialameijer@hotmail.com>
2018-05-08 17:22:36 +0200

    init commit from local

---
+++ Assets/Scripts/gameSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameSystem : MonoBehaviour {
+    //The gamesystem class does count the points of the teams, and builds the grid for the board. 
+    // Use this for initialization
+    public int Mapsize;
+    public float outlineSize;
+    //public Transform TilePrefab;
+    public Transform[] tiles;
+    
+
+
+    void Start () {
+        
+        MapGenererator();
+	}
+    void MapGenererator()
+    {
+        
+        string holderName = "Map";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int x = 0; x < Mapsize; x++)
+        {
+            for (int y = 0; y < Mapsize; y++)
+            {
+                int randomnum = Random.Range(0, 5);
+                Vector3 position = new Vector3(-Mapsize / 2 + 0.5f + x, 0, -Mapsize / 2 + 0.5f + y);
+                Transform newTile = Instantiate(tiles[randomnum], position, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlineSize );
+                newTile.parent = mapHolder;
+                print(randomnum);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/rollingDice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class rollingDice : MonoBehaviour {
+    public float forceStrenght;
+    public float torqueStrenght;
+    public ForceMode forcemode;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Random.onUnitSphere * forceStrenght, forcemode);
+            rb.AddTorque(Random.onUnitSphere * torqueStrenght, forcemode);
+        }
+	}
+}
+    
