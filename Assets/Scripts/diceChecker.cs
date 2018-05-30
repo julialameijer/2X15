fd14d3a862fe62f7bafff48809879d4ec10d57c1
Julia Lameijer <julialameijer@hotmail.com>
2018-05-30 09:56:11 +0200

    checks on what pit the dice lands on

---
+++ Assets/Scripts/diceChecker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diceChecker : MonoBehaviour
+{
+    private rollingDice rd;
+    string points;
+    int thrownPoints = 0;
+    // Use this for initialization
+    void Start()
+    {
+        points = this.tag;
+        rd = GameObject.FindObjectOfType<rollingDice>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(rd.Dicetimer <= 0)
+        {
+            if (col.gameObject.CompareTag("Floor"))
+            {
+                thrownPoints = int.Parse(points);
+                print(thrownPoints);
+            }
+        }
+    }
+}
